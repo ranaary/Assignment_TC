dd5e9ee180a67d98220b52b8cb8045eaf2612c84
Aryan <ranaary@sheridancollege.ca>
2022-04-18 16:33:22 -0400

    Rank.java and Suit.java created

---
+++ src/wargameproject/Rank.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wargameproject;
+
+/**
+ *
+ * @author ranaa
+ */
+public enum Rank {
+//    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
---
+++ src/wargameproject/Suit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wargameproject;
+
+/**
+ *
+ * @author ranaa
+ */
+public enum Suit {
+    Hearts,
+    Diamonds,
+    Spades,
+    Clubs,
+}
